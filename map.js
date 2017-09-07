b45c291cd6318c2e966e109c4a9de3516e68077d
kwan14 <mendel.kwan@telus.net>
2017-09-07 21:20:26 +0000

    Calculated hypotenuse of right triangles

---
+++ map.js
@@ -0,0 +1,14 @@
+
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map( function (element) { return Math.pow((Math.pow(element.x, 2) + Math.pow(element.y, 2)), 0.5); });
+
+console.log(result);
+
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
